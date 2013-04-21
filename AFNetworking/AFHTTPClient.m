1e0c2f4de84e17adf1d3945be4aae1675eeb229f
Robert Hedin <robert.hedin@weather.com>
2013-04-20 23:00:45 -0400

    corrected parameter name

--- AFNetworking/AFHTTPClient.m
+++ AFNetworking/AFHTTPClient.m
@@ -662,7 +662,7 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
                    success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
                    failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
 {
-	NSURLRequest *request = [self requestWithMethod:httpType
+	NSURLRequest *request = [self requestWithMethod:requestType
                                                path:path
                                          parameters:parameters];
     AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request
